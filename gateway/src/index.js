5bf7a26fc8a7a81df9cedb6ca9af160a18641972
root <root@ip-172-31-13-0.eu-west-3.compute.internal>
2023-12-30 15:50:40 +0000

    test

--- gateway/src/index.js
+++ gateway/src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const axios = require("axios");
 
+
+
 if (!process.env.PORT) {
     throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
 }
